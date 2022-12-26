7cdb598586c21e353e60266ed96c55917d6672d0
MariaAbdullah97 <meroo3066@gmail.com>
2022-12-26 23:25:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+let num1=31;
+let num2=2;
+let num3=12;
+let num4=3;
+//question 1
+let multiply=num1*num2;
+multiply=Math.ceil(multiply);
+console.log(multiply);
+
+//Question2
+let random=Math.floor(Math.random()*10)+1;
+console.log(random);
+//Question3
+let mod=num3/num4;
+console.log(mod);
+
+//Question4
+let max=Math.max(9,20,);
+console.log(max);
